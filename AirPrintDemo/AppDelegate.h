ba649fa9e08a16d24cfd9f7c4e4df7d58e792365
zhangyafeng123 <1240334911@qq.com>
2018-07-05 16:41:38 +0800

    Initial Commit

---
+++ AirPrintDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirPrintDemo
+//
+//  Created by linjianguo on 2018/7/5.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
